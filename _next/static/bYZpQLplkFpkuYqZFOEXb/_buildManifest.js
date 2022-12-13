d113101a548d0de5dcce80a3999967b282999672
kuderiavetsanton <70364196+kuderiavetsanton@users.noreply.github.com>
2022-12-13 09:01:44 +0000

    Deploying to gh-pages from @ kuderiavetsanton/synebo-test@3d88c1cc6bb45358a635b12cdb0fb765172dfddc 🚀

--- _next/static/bYZpQLplkFpkuYqZFOEXb/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/768-2301d4b0966b099f.js","static/css/ef46db3751d8e999.css","static/chunks/pages/index-eaa13a4f2576436c.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
